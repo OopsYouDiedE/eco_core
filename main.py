a17e29dea7f1856279c64ecf2583b42b5c595d7e
zhouzinimg <69280251+zhouzinimg@users.noreply.github.com>
2024-03-08 22:15:42 +0800

    fix

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ class Core(interactions.Extension):
         required=True,
         opt_type=interactions.OptionType.ROLE
     )
-    async def add_role(self, ctx: interactions.SlashContext, role: str):
+    async def add_role(self, ctx: interactions.SlashContext, role_id: str):
         await ctx.send(id_manager.add_id(str(role)))
 
     @module_base.subcommand("del_role", sub_cmd_description="删除管理员身份组。")
@@ -211,7 +211,7 @@ class Core(interactions.Extension):
         required=True,
         opt_type=interactions.OptionType.ROLE
     )
-    async def del_role(self, ctx: interactions.SlashContext, role: str):
+    async def del_role(self, ctx: interactions.SlashContext, role_id: str):
         await ctx.send(id_manager.remove_id(str(role)))
 
 
