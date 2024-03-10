19f7d5964780596268de3bf2483c9cf90f5d35b2
Noop ! <69280251+OopsYouDiedE@users.noreply.github.com>
2024-03-10 13:54:04 +0800

    v3.2

--- main.py
+++ main.py
@@ -22,12 +22,10 @@ from interactions import AutocompleteContext
 from . import database_manager
 from . import market_manager
 import yaml
+
 '''
 The DEV_GUILD must be set to a specific guild_id
 '''
-
-
-
 
 
 # ID管理器类
@@ -64,9 +62,6 @@ class IDManager:
             return f'删除id{id_to_remove}'
         else:
             return '无此id，删除失败'
-
-
-
 
 
 class KeyValueManager:
@@ -115,6 +110,8 @@ coin_and_owner = KeyValueManager(f'{os.path.dirname(__file__)}/coin_and_owner.ya
 exchangeable_item.add_id('劳动券')
 
 
+<<<<<<< HEAD
+=======
 
 @Core.command_send_item.autocomplete('item')
 @Core.command_check_item.autocomplete('item')
@@ -137,6 +134,7 @@ async def items_option_module_autocomplete(ctx: interactions.AutocompleteContext
     )
 
 
+>>>>>>> 81c9e802650a0fb493038cd82c28293bf9d922f9
 class Core(interactions.Extension):
     module_base: interactions.SlashCommand = interactions.SlashCommand(
         name="core",
@@ -449,3 +447,27 @@ class SetExchangeItems(interactions.Extension):
     )
     async def del_item(self, ctx: interactions.SlashContext, item_name: interactions.Role):
         await ctx.send(exchangeable_item.remove_id(item_name))
+
+
+import main
+
+
+@main.Core.command_send_item.autocomplete('item')
+@main.Core.command_check_item.autocomplete('item')
+@main.Market.sell_item.autocomplete('item')
+@main.Market.sell_item.autocomplete('exchange_item')
+async def items_option_module_autocomplete(ctx: interactions.AutocompleteContext):
+    items_option_input: str = ctx.input_text
+    modules: list[str] = list(exchangeable_item.ids)
+    modules_auto: list[str] = [
+        i for i in modules if items_option_input in i
+    ]
+
+    await ctx.send(
+        choices=[
+            {
+                "name": i,
+                "value": i,
+            } for i in modules_auto
+        ]
+    )
